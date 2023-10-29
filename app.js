6cdc9e82cf28aa7470bc17fb4b10663a5328f935
manoj809000 <116932159+manoj809000@users.noreply.github.com>
2023-10-29 15:29:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.post('/dashboard', (req, res) => {
 
 
 // Start your server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
